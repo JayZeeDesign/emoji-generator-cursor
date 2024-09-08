668be018d994fb823ed1443239e89deab7017a87
Jason Zhou <jason.zhou.design@gmail.com>
2024-09-08 20:46:41 +1000

    added signin

--- emoji-maker/middleware.ts
+++ emoji-maker/middleware.ts
@@ -8,7 +8,7 @@ export default clerkMiddleware(async (auth, req) => {
 
     // If the user isn't signed in and the route is private, redirect to sign-in
     if (!userId && isProtectedRoute(req)) {
-        return redirectToSignIn({ returnBackUrl: "/" });
+        return NextResponse.redirect(new URL('/sign-in', req.url));
     }
 
     // If the user is logged in and the route is protected, let them view.
